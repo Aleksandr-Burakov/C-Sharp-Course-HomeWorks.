7fdefe815c2b4667247e7bf4158e7c07f53b97ac
Aleksandr Burakov <aleks-bur-git@yandex.ru>
2022-12-17 21:15:22 +0300

    Написал Код

---
+++ HomeWorks/HomeWork_Lesson_04/Se_04_Dz_27/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+// Строки использовать нельззя
+
+
+using static System.Console;
+Clear();
+Write("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+int length = NumberLength(number);
+SummaNumbers(number, length);
+int NumberLength(int a)
+{
+    int i = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        i++;
+    }
+    return i;
+}
+void SummaNumbers(int n, int len)
+{
+    int summa = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        summa += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(summa);
+}
+
+
