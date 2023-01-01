e94a5293c0c9e2e7260ddca9e78e60eabdcc3e7e
Aleksandr Burakov <aleks-bur-git@yandex.ru>
2023-01-01 10:58:17 +0300

    Написал код

---
+++ HomeWorks/HomeWork_Lesson_05/Se_05_Dz_/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+using static System.Console;
+Clear();
+WriteLine("Введите длинну массива: ");
+int lenArray = Convert.ToInt32(ReadLine());
+int[] randomArray = new int[lenArray];
+int count = 0;
+FillArrayRandomNumbers(randomArray);
+PrintArray(randomArray);
+
+for (int i = 0; i < randomArray.Length; i++)
+{
+    if (randomArray[i] % 2 == 0) 
+    count++;
+}
+
+WriteLine($"четные числа в массиве -> {count} ");
+
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Write(array[i] + " ");
+    }
+    WriteLine();
+}
