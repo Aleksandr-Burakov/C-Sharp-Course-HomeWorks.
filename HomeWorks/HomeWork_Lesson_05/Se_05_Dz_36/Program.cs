85bf55c1b7047758b80aa6ce37672ad76241d1c4
Aleksandr Burakov <aleks-bur-git@yandex.ru>
2023-01-01 11:44:44 +0300

    Написал код к Se_05_Dz_36

---
+++ HomeWorks/HomeWork_Lesson_05/Se_05_Dz_36/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+using static System.Console;
+Clear();
+int size = 4;
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int sumNumbers = 0;
+for (int i = 0; i < numbers.Length; i += 2)
+{
+    sumNumbers += numbers[i];
+}
+WriteLine(sumNumbers);
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 100);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Write(array[i] + " ");
+    }
+    WriteLine();
+}
