6146caaefc383340fff61d8ca87bb95e5c950bba
Aleksandr Burakov <aleks-bur-git@yandex.ru>
2023-01-01 12:14:20 +0300

    Написал код к Se_05_Dz_38

---
+++ HomeWorks/HomeWork_Lesson_05/Se_05_Dz_38/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+using static System.Console;
+Clear();
+int size = 5;
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int max = numbers[0];
+int min = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+
+    else if (numbers[i] > min)
+    {
+        min = numbers[i];
+    }
+
+}
+WriteLine($"{max}");
+WriteLine($"{min}");
+WriteLine($"Разница между Max и Min числом : {max - min}");
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Write(array[i] + " ");
+    }
+    WriteLine();
+}
