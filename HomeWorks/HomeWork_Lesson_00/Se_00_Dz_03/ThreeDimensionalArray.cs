a4f213bccb273e72d8bd19c77c1faeb727d6a864
Aleksandr Burakov <aleks-bur-git@yandex.ru>
2023-01-08 21:00:40 +0300

    Полезный код добавил в Se_00_Dz_00-04

---
+++ HomeWorks/HomeWork_Lesson_00/Se_00_Dz_03/ThreeDimensionalArray.cs
@@ -0,0 +1,64 @@
+﻿//Метод создания Трехмерного массива 2*2*2 с выводом индекса для каждого элемента массива
+//Генерируя неповторяемые рандомные значения для элемента массива.
+
+
+using static System.Console;
+Clear();
+int[,,] ThreeDimensionalArray = new int[2, 2, 2];
+
+// Генератор неповторяющихся чисел. 
+// Метод генерации с сохранением в HashSet,
+// проверяя при последуещей генерации был ли элемент получен ранее:
+
+HashSet<int> numbers = new HashSet<int>();
+Random randomNum = new Random();
+int GenerateUniqueNumber()
+{
+    while (true)
+    {
+        var num = randomNum.Next(10, 19);
+        if (!numbers.Contains(num))
+        {
+            numbers.Add(num);
+            return num;
+        }
+    }
+}
+
+// Метод заполнения Трёхмерного массива случайными неповторяющимися числами 
+//с использованием локального генератора неповторяющихся чисел.
+
+void FileArray(int[,,] array)
+{
+    for (int i = 0; i < ThreeDimensionalArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < ThreeDimensionalArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < ThreeDimensionalArray.GetLength(2); k++)
+            {
+                ThreeDimensionalArray[i, j, k] = GenerateUniqueNumber();
+            }
+        }
+    }
+}
+
+// Метод вывода Трёхмерного массива в Консоль терминала.
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < ThreeDimensionalArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < ThreeDimensionalArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < ThreeDimensionalArray.GetLength(2); k++)
+            {
+                Write($"{ThreeDimensionalArray[i, j, k]} ({j}, {k}, {i}) ");
+            }
+            WriteLine();
+        }
+        WriteLine();
+    }
+}
+FileArray(ThreeDimensionalArray);
+PrintArray(ThreeDimensionalArray);
+
---
+++ HomeWorks/HomeWork_Lesson_00/Se_00_Dz_04/GenerateUniqueNumber.cs
@@ -0,0 +1,39 @@
+﻿// Размерность массива не играет никакой роли.
+// Сверяемся со значениями, которые уже находятся в массиве 
+// или сохранять сгенерированные элементы куда-либо,
+// например в Hashset<T>, и проверять при последуещей генерации был ли элемент получен ранее.
+// Метод генерации с сохранением в HashSet,
+// проверяя при последуещей генерации был ли элемент получен ранее 
+// будет выглядеть примерно так:
+
+
+static HashSet<int> numbers = new HashSet<int>();
+static Random r = new Random();
+
+public static int GenerateUniqueNumber()
+{
+    while (true)
+    {
+        var n = r.Next(0, 1000);
+        if (!numbers.Contains(n))
+        {
+            numbers.Add(n);
+            return n;
+        }
+    }
+}
+
+// Само заполнение Трёхмерного массива будет выглядеть так:
+
+var arr = new int[5,5,5];
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        for (int k = 0; k < 5; k++)
+        {
+            arr[i, j, k] = GenerateUniqueNumber();
+        }
+    }
+}
