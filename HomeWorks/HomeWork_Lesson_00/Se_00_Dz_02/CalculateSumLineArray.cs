408347f5c14f0145f10834b37511a8fa2d85d0ed
Aleksandr Burakov <aleks-bur-git@yandex.ru>
2023-01-08 20:11:00 +0300

    Написал код к Se_08_Dz_60

---
+++ HomeWorks/HomeWork_Lesson_00/Se_00_Dz_02/CalculateSumLineArray.cs
@@ -0,0 +1,63 @@
+﻿// Метод создание,заполнение Двумерного Прямоугольного массива рандомными числами,
+// Подсчет суммы строки и вывод минимальной суммы элементов строки с номером строки.
+ 
+// Программа считает сумму элементов в каждой строке и
+// выдаёт номер строки в которой наименьшая сумма элементов:
+
+// Минимальная сумма элементов = 34 и находится в 1 строке. 
+
+
+
+using static System.Console;
+Clear();
+int[,] RectangularArray = new int[4, 4];
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{RectangularArray[i, j]}  ");
+        }
+        WriteLine();
+    }
+}
+void FileArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(10, 50);
+        }
+    }
+}
+void CalculateSumLineArray(int[,] array)
+{
+    int row = 0;
+    int minsum = 0;
+    int sumrow = 0;
+    for (int i = 0; i < RectangularArray.GetLength(1); i++)
+    {
+        row = row + RectangularArray[0, i];
+    }
+    for (int i = 0; i < RectangularArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < RectangularArray.GetLength(1); j++)
+            sumrow = sumrow + RectangularArray[i, j];
+        if (sumrow < row)
+        {
+            row = sumrow;
+            minsum = i;
+        }
+        WriteLine($"   Сумма элементов строки = {sumrow}");
+        sumrow = 0;
+    }
+    WriteLine();
+    WriteLine($"Минимальная сумма элементов = {row} и находится в {minsum + 1} строке.");
+}
+FileArray(RectangularArray);
+PrintArray(RectangularArray);
+WriteLine();
+CalculateSumLineArray(RectangularArray);
+WriteLine();
---
+++ HomeWorks/HomeWork_Lesson_08/Se_08_Dz_60/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+
+using static System.Console;
+Clear();
+int[,,] ThreeDimensionalArray = new int[2, 2, 2];
+HashSet<int> numbers = new HashSet<int>();
+Random randomNum = new Random();
+int GenerateUniqueNumber()
+{
+    while (true)
+    {
+        var num = randomNum.Next(10, 19);
+        if (!numbers.Contains(num))
+        {
+            numbers.Add(num);
+            return num;
+        }
+    }
+}
+void FileArray(int[,,] array)
+{
+    for (int i = 0; i < ThreeDimensionalArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < ThreeDimensionalArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < ThreeDimensionalArray.GetLength(2); k++)
+            {
+                ThreeDimensionalArray[i, j, k] = GenerateUniqueNumber();
+            }
+        }
+    }
+}
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < ThreeDimensionalArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < ThreeDimensionalArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < ThreeDimensionalArray.GetLength(2); k++)
+            {
+                Write($"{ThreeDimensionalArray[i, j, k]} ({j}, {k}, {i},) ");
+            }
+            WriteLine();
+        }
+        WriteLine();
+    }
+}
+FileArray(ThreeDimensionalArray);
+WriteLine();
+PrintArray(ThreeDimensionalArray);
+WriteLine();
